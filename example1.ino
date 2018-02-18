8a233ca368fb0358f5f373f122d7af796493af8b
nahlatarek <36340819+nahlatarek@users.noreply.github.com>
2018-02-19 01:41:50 +0200

    examle2

---
+++ example1.ino
@@ -0,0 +1,41 @@
+  int ledr=4;
+  int ledw=6;
+  int ledg=5; 
+  int ledy=7; 
+  
+  
+  
+  void setup(){
+pinMode(ledr,OUTPUT);
+pinMode(ledw,OUTPUT);
+pinMode(ledg,OUTPUT);
+pinMode(ledy,OUTPUT);
+
+}
+
+void loop() {
+  
+
+  
+  while(1)
+    {
+      digitalWrite(ledr,HIGH);
+      delay(1000);
+      digitalWrite(ledr,LOW);
+    digitalWrite(ledw,HIGH);
+    delay(1000);
+    digitalWrite(ledw,LOW);
+    digitalWrite(ledg,HIGH);
+    delay(1000);
+    digitalWrite(ledg,LOW);
+    digitalWrite(ledy,HIGH);
+    delay(1000);
+    digitalWrite(ledy,LOW);
+    
+  
+}
+ 
+ 
+  
+
+}
