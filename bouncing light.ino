e8d5cef4d87d0df3dac5b292fde331b488e0f582
christeenEmad <36341249+christeenEmad@users.noreply.github.com>
2018-02-20 01:00:06 +0200

    Add files via upload

---
+++ bouncing light.ino
@@ -0,0 +1,56 @@
+int red = 4;
+int blue = 5;
+int yellow = 6;
+int green = 7;
+void setup()
+{
+  pinMode(red , OUTPUT);
+  pinMode(blue , OUTPUT);
+  pinMode(yellow , OUTPUT);
+  pinMode(green , OUTPUT);
+}
+void loop ()
+{  
+  digitalWrite(red , HIGH);
+  digitalWrite(blue , LOW);
+  digitalWrite(yellow , LOW);
+  digitalWrite(green , LOW);
+  delay(1000);
+  digitalWrite(red , LOW);
+  digitalWrite(blue , HIGH);
+  digitalWrite(yellow , LOW);
+  digitalWrite(green , LOW);
+  delay(1000);
+  digitalWrite(red , LOW);
+  digitalWrite(blue , LOW);
+  digitalWrite(yellow , HIGH);
+  digitalWrite(green , LOW);
+  delay(1000);
+  digitalWrite(red , LOW);
+  digitalWrite(blue , LOW);
+  digitalWrite(yellow , LOW);
+  digitalWrite(green , HIGH);
+  delay(1000);
+  
+   digitalWrite(red , LOW);
+  digitalWrite(blue , LOW);
+  digitalWrite(yellow , LOW);
+  digitalWrite(green , HIGH);
+  delay(1000);
+  digitalWrite(red , LOW);
+  digitalWrite(blue , LOW);
+  digitalWrite(yellow , HIGH);
+  digitalWrite(green , LOW);
+  delay(1000);
+  digitalWrite(red , LOW);
+  digitalWrite(blue , HIGH);
+  digitalWrite(yellow , LOW);
+  digitalWrite(green , LOW);
+  delay(1000);
+  digitalWrite(red , HIGH);
+  digitalWrite(blue , LOW);
+  digitalWrite(yellow , LOW);
+  digitalWrite(green , LOW);
+  delay(1000);
+
+}
