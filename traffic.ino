9c2da8b4b1c4810861627bf33b83e78acbbd6c3c
christeenEmad <36341249+christeenEmad@users.noreply.github.com>
2018-02-20 01:14:28 +0200

    Add files via upload

--- traffic.ino
+++ traffic.ino
@@ -1,19 +1,26 @@
-
-
-void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+int yellow = 12;
+int green = 13;
+int red = 11;
+void setup()
+{
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT);
 }
-
-void loop() {
-  digitalWrite(2, HIGH);  
-  delay(1000);              
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  delay(1000);
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);
-  delay(1000);
-  digitalWrite(4, LOW);              
-}
+  void loop()
+  {
+    digitalWrite(green, HIGH);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, LOW);
+    delay(3000); 
+    
+     digitalWrite(green, LOW);
+    digitalWrite(yellow, HIGH);
+    digitalWrite(red, LOW);
+    delay(3000);
+    
+     digitalWrite(green, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(red, HIGH);
+    delay(3000);
+  }
