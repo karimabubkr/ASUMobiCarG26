f6c750f05e4695f4ca718bde53b57da53f7c2e6d
nahlatarek <36340819+nahlatarek@users.noreply.github.com>
2018-02-19 12:29:41 +0200

    example 3

---
+++ example_for_pwm_prop..ino
@@ -0,0 +1,27 @@
+
+
+  
+  
+  
+  int ledr=4;
+  int x;
+  void setup(){
+pinMode(ledr,OUTPUT);
+
+}
+
+void loop() {
+  
+
+for(x=0;x<=255;x++)
+    {
+      analogWrite(ledr,x);
+      delay(50);
+    }  
+  
+    for(x=255;x>=0;x--)
+    {
+      analogWrite(ledr,x);
+      delay(50);
+    }
+}
