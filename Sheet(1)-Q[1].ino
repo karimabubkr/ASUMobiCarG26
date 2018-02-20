59a54779a3a904a1255baf33469a0a3053e73487
omarkhaledf <36339238+omarkhaledf@users.noreply.github.com>
2018-02-20 20:18:45 +0200

    Add files via upload
    sheet (1) -- question [1] solution

---
+++ Sheet(1)-Q[1].ino
@@ -0,0 +1,18 @@
+
+//sheet 1 - question 1 solution
+
+void setup() 
+{
+
+ pinMode(5,OUTPUT); 
+
+ //defining the led pin as output.
+
+}
+void loop()
+ {
+digitalWrite(5,HIGH);
+delay(500);
+digitalWrite(5,LOW);
+delay(1500);
+}
