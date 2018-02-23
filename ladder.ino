74798009d5e109ff6b865f9c2e71df4590c80e95
QassemAhmed <36255719+QassemAhmed@users.noreply.github.com>
2018-02-23 16:18:56 +0200

    ladder examble for 3 floors

---
+++ ladder.ino
@@ -0,0 +1,22 @@
+int i=8,j=5;
+void setup() 
+{
+  for(;i<11&j<8;i++)
+  {
+    pinMode(i,OUTPUT);
+    pinMode(j,INPUT);
+    j++;
+  }
+}
+
+void loop() 
+{
+  if(digitalRead(5)== HIGH||digitalRead(6)== HIGH||digitalRead(7)== HIGH)
+    {for(i=8;i<11;i++)
+      digitalWrite(i,HIGH);  
+     delay(10000);
+     for(i=8;i<11;i++)
+      digitalWrite(i,LOW);  
+    } 
+}
+
