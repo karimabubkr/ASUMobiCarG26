ca24b25aeb7b1fffec7170fcbc246e81bfe74bdf
zeyadelsayed <36342095+zeyadelsayed@users.noreply.github.com>
2018-02-24 15:17:37 +0200

    Arduino code example.
    example about dimming and brightening a led using two push buttons.

---
+++ lightDimmingAndBrighting.ino
@@ -0,0 +1,35 @@
+int i = 0;
+int led = 3;
+int button1 = 9;
+int button2 = 10;
+
+void setup() {
+  pinMode(button1, INPUT);   
+  pinMode(button2, INPUT); 
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  analogWrite(led, i);  // analogWrite values from 0 to 255
+
+  //brighting
+  if (digitalRead(button1)==LOW)
+  {
+    if (i<255)
+    {
+      i++;
+      delay(30);
+      }
+  }
+
+  //dimming
+  if (digitalRead(button2)==LOW)
+  {
+    if (i>0)
+    {
+      i--;// 
+      delay(30);
+    }
+  }
+}
