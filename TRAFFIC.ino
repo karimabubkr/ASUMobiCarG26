218234f45ffdded23acb0685b60d4c7276044807
karimabubkr <36203541+karimabubkr@users.noreply.github.com>
2018-03-04 00:18:35 +0200

    Add files via upload
    trafic leds

---
+++ TRAFFIC.ino
@@ -0,0 +1,24 @@
+int yellow =13;
+int green =12;
+int red =11;
+void setup()
+{
+  pinMode(yellow,OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+void loop()
+{
+ digitalWrite(yellow,HIGH);
+ digitalWrite(red,LOW);
+ digitalWrite(green,LOW);
+ delay(2000);
+ digitalWrite(yellow,LOW);
+ digitalWrite(red,HIGH);
+ digitalWrite(green,LOW);
+ delay(2000);
+ digitalWrite(yellow,LOW);
+ digitalWrite(red,LOW);
+ digitalWrite(green,HIGH);
+}
+ 
