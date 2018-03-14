addf53b838b463013ce11867686997b7624ec12e
QassemAhmed <36255719+QassemAhmed@users.noreply.github.com>
2018-03-14 20:58:18 +0200

    ultrasonic sensor

---
+++ Ultra.ino
@@ -0,0 +1,20 @@
+const byte Trig = 7;
+const byte Echo = 8;
+float distance ;
+unsigned long period;
+void setup() {
+ pinMode(Trig,OUTPUT);
+ pinMode(Echo,INPUT);
+ Serial.begin(9600);
+}
+void loop() {
+digitalWrite(Trig,LOW);
+digitalWrite(Trig,HIGH);
+delayMicroseconds(10);
+digitalWrite(Trig,LOW);
+period = pulseIn(Echo,HIGH);
+distance = float(period)*0.017;             //distace = (period/2*10^6)*340*100 cm 
+Serial.println(distance);
+delay(60);
+
+}
