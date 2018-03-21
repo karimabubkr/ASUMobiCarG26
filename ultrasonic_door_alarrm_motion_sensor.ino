653389b6a704acb3c3869ee588566f43924e82f4
karim ELballah <36312011+karim150@users.noreply.github.com>
2018-03-21 23:43:56 +0200

    Update ultrasonic_door_alarrm_motion_sensor.ino

--- ultrasonic_door_alarrm_motion_sensor.ino
+++ ultrasonic_door_alarrm_motion_sensor.ino
@@ -2,60 +2,45 @@ int trigPin = 12;
 int echoPin = 13;
 int Buzzer = 8;
 int duration, distance;
-
 void setup() 
 {
-
 pinMode(trigPin, OUTPUT);
 pinMode(echoPin, INPUT);
 pinMode(Buzzer, OUTPUT);
 Serial.begin (9600);
-
 }
-
 void loop() 
 {
-  
-
 digitalWrite(trigPin, HIGH);
 delayMicroseconds(1000);
-
 digitalWrite(trigPin, LOW);
 duration = pulseIn(echoPin, HIGH);
 distance = (duration/2) / 29.1;
 
-if (distance >= 80 || distance <= 0)
+  if (distance >= 80 || distance <= 0)
 {
 Serial.println("no object detected");
 digitalWrite(Buzzer, LOW);
-
 }
-
+  
 else 
 {
 Serial.println("object detected");
 tone(Buzzer, 400); // play 400 Hz tone for 500 ms
 delay(500);
-
 tone(Buzzer, 800); // play 800Hz tone for 500ms
 delay(500);
-
 tone(Buzzer, 400); // play 400 Hz tone for 500 ms
 delay(500);
-
 tone(Buzzer, 800); // play 800Hz tone for 500ms
 delay(500);
-
 tone(Buzzer, 400); // play 400 Hz tone for 500 ms
 delay(500);
-
 tone(Buzzer, 800); // play 800Hz tone for 500ms
 delay(500);
 noTone(Buzzer);
 }
-
 delay(300);
-
 }
 
 
