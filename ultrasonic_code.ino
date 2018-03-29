93ac040f17823bb4794f6b9f3a9ea03de20fcb88
nahlatarek <36340819+nahlatarek@users.noreply.github.com>
2018-03-29 21:48:01 +0200

    ultrasonic code

---
+++ ultrasonic_code.ino
@@ -0,0 +1,25 @@
+int trigPin=12;
+int echoPin=13;
+int Time, distance;
+void setup()
+{
+    Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+ 
+
+}
+
+void loop() {
+ digitalWrite(trigPin, HIGH); 
+ delayMicroseconds(3); 
+ digitalWrite(trigPin, LOW); 
+ Time = pulseIn(echoPin, HIGH);
+ digitalWrite(echoPin,LOW);
+ 
+distance = (Time/29)/2;
+  Serial.print(distance);   
+  Serial.println(" cm ");
+  delay(50);
+
+}
