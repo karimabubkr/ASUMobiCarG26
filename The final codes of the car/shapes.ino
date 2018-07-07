f477c4366b2c5f81a1196c2dff6add35203671ef
Kareem Osama <kimo_os@aol.com>
2018-07-07 21:16:05 +0200

    Update shapes.ino

--- The final codes of the car/shapes.ino
+++ The final codes of the car/shapes.ino
@@ -4,22 +4,32 @@
  * 
 */
 
+
 int led = 13;
+
 //left motors
+
 int enl = 5;//enable left
 int pin6 = 6;//B2
 int pin7 = 7;//B1
+
 //right motors
+
 int enr = 10;//enable left
 int pin8 = 8;//A2
 int pin9 = 9;//A1
 
 //bluetooth 
+
 char BluetoothData;
 
-void setup(){
+void setup()
+{
   pinMode(led,OUTPUT);
-  //motor
+
+  
+ //motor
+ 
   pinMode(pin6,OUTPUT);
   pinMode(pin7,OUTPUT);
   pinMode(pin8,OUTPUT);
@@ -32,15 +42,18 @@ void setup(){
   }
 
 // Driving functions
+
 void Stop(){
-digitalWrite(led,LOW);
+
+  digitalWrite(led,LOW);
 digitalWrite(pin6,LOW);
 digitalWrite(pin7,LOW);
 digitalWrite(pin8,LOW);
 digitalWrite(pin9,LOW);
 }
 
-void turn_right(){
+void turn_right()
+{
 digitalWrite(led,HIGH);
 digitalWrite(pin6,HIGH);
 digitalWrite(pin7,LOW);
@@ -48,7 +61,8 @@ digitalWrite(pin8,LOW);
 digitalWrite(pin9,LOW);
 }
   
-void forward(){
+void forward()
+{
 digitalWrite(led,HIGH);
 digitalWrite(pin6,HIGH);
 digitalWrite(pin7,LOW);
@@ -64,7 +78,8 @@ digitalWrite(pin8,LOW);
 digitalWrite(pin9,HIGH);
 }
 
-void turn_left(){
+void turn_left()
+{
 digitalWrite(led,HIGH);
 digitalWrite(pin6,LOW);
 digitalWrite(pin7,LOW);
@@ -73,7 +88,9 @@ digitalWrite(pin9,LOW);
 }
 
 //shapes functions
-void shapes(){
+
+void shapes()
+{
 
   //Rectangle shape function
   analogWrite(enl,160);
@@ -113,6 +130,7 @@ void shapes(){
   delay(5000);
   
 // circle Right shape function
+
   forward();
   delay(30);
   
@@ -129,6 +147,7 @@ void shapes(){
   delay(5000);
  
 //inf shape function
+  
   forward();
   delay(50);
   
@@ -155,7 +174,8 @@ void shapes(){
 }
 
 
-void loop() {
+void loop()
+{
   BluetoothData = Serial.read();
   digitalWrite(enl,HIGH);
   digitalWrite(enr,HIGH);
